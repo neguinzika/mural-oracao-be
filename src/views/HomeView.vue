3f0cdf9e283cb2c579f76d56076eebff1385cf89
Dennys Fredericci <dfredericci@mazdaeur.com>
2024-02-20 18:15:14 +0100

    arrumei o botao

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,18 +4,16 @@
     <h1>
       Faça seu pedido de Oração
     </h1>
+
+    <form @submit.prevent="submitForm">
     <label for="name" class="form-label"> Nome: </label>
       <div>
       <input  class="form-control" v-model="name" placeholder="Digite seu nome" required>
       </div>
     <label for="oracao" class="form-label"> Pedido de Oração:</label><br>
-
     <textarea class="form-control" v-model="oracao" rows="9" placeholder="Digite aqui seu pedido de Oração"></textarea>
-
     <br><br>
-
-    <form>
-    <button  v-on:click="submitForm" style=color:blue;>Enviar Pedido</button>
+      <button style=color:blue;>Enviar Pedido</button>
     </form>
 
   </div>
@@ -24,21 +22,27 @@
 
 <script>
 
-import {collection ,addDoc , } from 'firebase/firestore'
 import 'firebase/firestore'
-import {db} from "@/firebase/index.js";
 
 
 export default {
 
+  data() {
+    return {
+      name: '',
+      oracao: ''
+    }
+  },
+
   methods: {
     async submitForm() {
 
-      const docRef = await addDoc(collection(db,"pedidos"),{
+      console.log(this.name)
+
+      /*const docRef = await addDoc(collection(db,"pedidos"),{
         name: this.name,
         oracao: this.oracao
-
-      })
+      })*/
 
     }
 
