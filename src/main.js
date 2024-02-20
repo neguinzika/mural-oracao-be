12ef9f90b7d3cf1868d778d71010f49b2a6c3733
Dennys Fredericci <dfredericci@mazdaeur.com>
2024-02-20 17:37:39 +0100

    cleanup

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import './assets/main.css'
-import {app} from "./firebase.js"
-import {initializeApp} from "firebase/app";
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
